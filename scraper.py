5bbd3d4720a89550a3ceac39d2943f35378bc06b
uzair-shah <s.uzair.shah01@gmail.com>
2026-05-13 17:38:41 +0930

    moving df to scraper

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from urllib.parse import urljoin
 import time
+import sqlite3
 
 data = []
 page_num = 1
@@ -10,24 +11,29 @@ page_num = 1
 while True:
     url = f"https://books.toscrape.com/catalogue/page-{page_num}.html"
     response = requests.get(url)
-    response.encoding = 'utf-8'
-    #shortcircuit if end of pages reached
     if response.status_code != 200:
         print(f"Stopped at page {page_num} (status {response.status_code})")
         break
     
-    url = f"https://books.toscrape.com/catalogue/page-{page_num}.html"
     #Extract page
-    response = requests.get(url)
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, 'html.parser')
     books = soup.find_all('article',class_ = 'product_pod') #List of all books in html format
+    
+    word_2_num = {'One':1, 'Two': 2, 'Three':3, 'Four': 4, 'Five':5}
+    
+
     #Parses single page from html to df
     for book in books:
         name = book.find('h3').find('a')['title'] #name of book
         price = float(book.find('div', class_ = 'product_price').find('p', class_ = 'price_color').text.strip("£")) #book price
         in_stock = book.find('p', class_ = 'instock availability').text.strip() #instock
-        rating = book.find('p')['class'][1] #star rating
+        
+        rating_words = book.find('p')['class'][1] #star rating
+        rating = 0
+        if rating_words in word_2_num: #Converting to integer value
+            rating = word_2_num[rating_words]
+
         rel_link = book.find('div',class_ = 'image_container').find('a').find('img')['src'] #relative link
         link = urljoin(url,rel_link)
         data.append({'name':name, 
@@ -40,5 +46,14 @@ while True:
     page_num += 1 #go to next page
     time.sleep(0.5) #wait for website
 df = pd.DataFrame(data)
-df = pd.DataFrame(data)
-print(df)
+
+
+#Linking dataframe to sql 
+conn = sqlite3.connect("books.db" ) #connection to an SQLite database called books.
+
+with sqlite3.connect("books.db") as conn:
+    df.to_sql('books',conn, if_exists = "replace",index = False)
+
+    #Checking if sql query works
+    result = pd.read_sql("SELECT * FROM books WHERE price < 20 ORDER BY price", conn)
+print(result.head())
